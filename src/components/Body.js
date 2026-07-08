f74492276cfe073ce686a8b5c159f955e21100dd
Zayeem Mohd <zayeemmohd@Zayeems-MacBook-Pro.local>
2026-07-08 19:02:52 +0530

    fetch using button

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,9 +3,22 @@ import RestaurantCard from "./RestaurantCard";
 import { Shimmer } from "./Shimmer";
 
 const Body = () => {
+  const getData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.374638644228302&lng=78.4300148114562&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="body">
-      <Shimmer/>
+      <div style={{ margin: "100px" }}>
+        {" "}
+        <button onClick={getData}>Get data</button>{" "}
+      </div>
+
+      {/* <Shimmer /> */}
 
       <div className="res-container">
         {/* {restaurantsArr.map((resObj) => {
