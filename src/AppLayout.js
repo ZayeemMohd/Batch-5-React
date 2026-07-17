70ff851b218ec1a48c237ef3ab5172e39a543777
Zayeem Mohd <zayeemmohd@Zayeems-MacBook-Pro.local>
2026-07-17 18:26:41 +0530

    Context Api, simple provider using .Provider

--- src/AppLayout.js
+++ src/AppLayout.js
@@ -2,16 +2,42 @@ import { Outlet } from "react-router";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import UserContext from "./utils/UserContext";
+import { useState, useEffect } from "react";
 
- 
- const AppLayout = () => {
+const AppLayout = () => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+
+  //authentication
+  useEffect(() => {
+    const randomTrue = Math.random() < 0.3;
+
+    if (!randomTrue) {
+      setUsername("guest");
+      setEmail("guestEmail.com");
+      return;
+    }
+
+    // data from backend if authentication call success
+    const loggedInUser = {
+      email: "zayeemuddin123@gmail.com",
+      name: "ZAYEEM",
+    };
+
+    setUsername(loggedInUser.name);
+    setEmail(loggedInUser.email);
+  }, []);
+
   return (
     <div>
-      <Header />
+      <UserContext.Provider value={{ name: username, email: email }}>
+        <Header />
         <Outlet />
-      <Footer />
+        <Footer />
+      </UserContext.Provider>
     </div>
   );
 };
 
-export default AppLayout; 
+export default AppLayout;
